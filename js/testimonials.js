5633fffcbf9cfa3db3dd41a8bb6a5c68670787d4
amrimarihotjati <amrimarihotjati@gmail.com>
2023-08-22 14:06:53 +0700

    remove some variables

--- js/testimonials.js
+++ js/testimonials.js
@@ -1,56 +1,5 @@
 
-// let dataTestimonials = [
-//     {
-//         image : "https://media.formula1.com/image/upload/t_16by9South/f_auto/q_auto/v1692021567/fom-website/2023/August%20Break/GettyImages-1252687923.jpg.transform/9col-retina/image.jpg",
-//         quote : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore pariatur ea possimus provident explicabo.",
-//         author : "Amri",
-//         rating : 2
-//     },
-//     {
-//         image : "https://media.formula1.com/image/upload/t_16by9Centre/f_auto/q_auto/v1692029137/fom-website/2023/August%20Break/GettyImages-1247794161.jpg.transform/9col-retina/image.jpg",
-//         quote : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore pariatur ea possimus provident explicabo.",
-//         author : "Izzan",
-//         rating : 2
-//     },
-//     {
-//         image : "https://media.formula1.com/image/upload/t_16by9North/f_auto/q_auto/v1691841385/fom-website/2023/August%20Break/GettyImages-1488026858.jpg.transform/9col-retina/image.jpg",
-//         quote : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore pariatur ea possimus provident explicabo.",
-//         author : "Nauvalsev",
-//         rating : 3
-//     },
-//     {
-//         image : "https://media.formula1.com/image/upload/t_16by9Centre/f_auto/q_auto/v1691597307/fom-website/2023/Red%20Bull/GettyImages-1248220932.jpg.transform/9col-retina/image.jpg",
-//         quote : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore pariatur ea possimus provident explicabo.",
-//         author : "Elfady",
-//         rating : 4
-//     },
-//     {
-//         image : "https://media.formula1.com/image/upload/v1690734334/trackside-images/2023/F1_Grand_Prix_of_Belgium/1580921322.jpg.transform/9col-retina/image.jpg",
-//         quote : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore pariatur ea possimus provident explicabo.",
-//         author : "Jati",
-//         rating : 3
-//     },
-//     {
-//         image : "https://static01.nyt.com/images/2021/12/16/multimedia/16sp-review-next-inyt1/16sp-review-next-inyt1-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
-//         quote : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore pariatur ea possimus provident explicabo.",
-//         author : "Verstappen",
-//         rating : 3
-//     },
-//     {
-//         image : "https://cdn.antaranews.com/cache/1200x800/2022/08/20/IMG-F1-1-01.jpeg.webp",
-//         quote : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore pariatur ea possimus provident explicabo.",
-//         author : "Alonso",
-//         rating : 4
-//     },
-//     {
-//         image : "https://assets.kompasiana.com/items/album/2023/05/24/1045223-646e166e822199339709c2f4.jpg?t=o&v=400",
-//         quote : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore pariatur ea possimus provident explicabo.",
-//         author : "Raikonen",
-//         rating : 5
-//     }
-// ]
-
-
+//get data from JSON
 const dataTestimonials = new Promise((resolve,reject) => {
 
     const newData = new XMLHttpRequest()
@@ -75,7 +24,6 @@ const dataTestimonials = new Promise((resolve,reject) => {
 
 // Show All Testimonials
 async function allTestimonials(){
-
     try {
         const dataJSON = await dataTestimonials
         console.log(dataJSON)
@@ -118,8 +66,9 @@ async function allTestimonials(){
 allTestimonials()
 
 
-async function filterTestimonials(rating){
 
+//FilteredTestimonials
+async function filterTestimonials(rating){
     try {
 
         const dataJSON =  await dataTestimonials
